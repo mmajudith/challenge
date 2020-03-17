9788fbe756e41d1956b3bf5d71231b0c11ddf2f7
mma judith <mmajudith2000@gmail.com>
2020-03-16 21:51:12 -0700

    adding javascript

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 'use strict';
 
+// Variables Declarations
+
+let fullName = document.querySelector('#fullName').value;
+let email = document.querySelector('#email').value;
+let submit = document.querySelector('submit');
+
+// Function Declaration 
+
+function formValidation(){
+    let nameRegx = /^judith$/;
+    // let mailRegx = //;
+
+    if(nameRegx.test(fullName)){
+        console.log("am Happly");
+     }else{
+         console.log("why na");
+    }
+}
+
+formValidation();
