06fab3c8dca8896c13d218fcc5490b2f186ade15
mma judith <mmajudith2000@gmail.com>
2020-03-22 11:46:41 -0700

    Form Validation

--- main.js
+++ main.js
@@ -2,21 +2,7 @@
 
 // Variables Declarations
 
-let fullName = document.querySelector('#fullName').value;
-let email = document.querySelector('#email').value;
-let submit = document.querySelector('submit');
+
+
 
 // Function Declaration 
-
-function formValidation(){
-    let nameRegx = /^judith$/;
-    // let mailRegx = //;
-
-    if(nameRegx.test(fullName)){
-        console.log("am Happly");
-     }else{
-         console.log("why na");
-    }
-}
-
-formValidation();
