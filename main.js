a52b05f47f7e3eefceddc2bf2404ae012f115308
mma judith <mmajudith2000@gmail.com>
2020-03-29 23:03:14 -0700

    Form Validation

--- main.js
+++ main.js
@@ -2,26 +2,29 @@
 
 // Variables Declarations
 
-const nameVal = document.getElementById('name-val').style.display = 'none';
-const mail = document.getElementById('mail').style.display = 'none';
-const pass = document.getElementById('pass').style.display = 'none';
+const nameVal = document.querySelector('#name-val').style.display = 'none';
+const mail = document.querySelector('#mail').style.display = 'none';
+const pass = document.querySelector('#pass').style.display = 'none';
 
-const fullName = document.getElementById('fullName');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const form = document.getElementById('form');
-
+// const fullName = document.querySelector('#fullName');
+// const email = document.querySelector('#email');
+// const password = document.querySelector('#password');
+const form = document.querySelector('#form');
 
 // Regx object patterns 
 
 const patterns = {
-    nRegx: /^[a-z]{3, 15}\s[a-z]{3, 15}$/i,
+    nRegx: /^[a-z]{3,15}\s[a-z]{3,15}$/gi,
     eRegx: /^[\w.]+@\w+\.(com|net|org)$/,
-    pRegx: /^[\w@-]{6, 20}$/
+    pRegx: /^[\w@.-]{6,20}$/
 };
 
 // Function
 
-form.addEventListener('submit', function(e){
-
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    console.log(form.fullName.value);
 });
+    
+    
+
